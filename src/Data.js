69f441551235b7eab52de42105af1c0aff172349
buribur <111722034+buribur@users.noreply.github.com>
2023-02-02 23:42:34 +0800

    Initial commit

---
+++ src/Data.js
@@ -0,0 +1,44 @@
+let shopItemsData = [
+  {
+    id: "product1",
+    name: "Earbuds",
+    price: 299,
+    desc: "Samsung Galaxy buds Pro here for your better sharing of musicANC (Active Noise Cancelling) is available!",
+    img: "Picture/samsung buds pro.jpeg",
+  },
+  {
+    id: "product2",
+    name: "Sony Headphone",
+    price: 299,
+    desc: "Sony Headphone here for your better sharing of musicANC (Active Noise Cancelling) is available!.",
+    img: "Picture/Headphone.jpg",
+  },
+  {
+    id: "product3",
+    name: "Earphone",
+    price: 25,
+    desc: "Earphone here for your better exprience of music.",
+    img: "Picture/earphone.jpeg",
+  },
+  {
+    id: "product4",
+    name: "KZ Earphone",
+    price: 799,
+    desc: "KZ Earphone here for your better exprience of music.",
+    img: "Picture/KZ earphone black .jpeg",
+  },
+  {
+    id: "product5",
+    name: "Airpod Pro",
+    price: 799,
+    desc: "Airpod Pro here for your better sharing of musicANC (Active Noise Cancelling) is available!.",
+    img: "Picture/airpod pro.jpeg",
+  },
+  {
+    id: "product5",
+    name: "JBL Speaker",
+    price: 799,
+    desc: "JBL Speaker here for your better exprience of music.",
+    img: "Picture/jbl speaker green .jpeg",
+  },
+];
